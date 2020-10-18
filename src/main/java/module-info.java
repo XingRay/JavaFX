43992c75912978380765962317adf1b9c6822c96
leixing <leixing1012@qq.com>
2020-10-19 00:41:29 +0800

    开放包

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module JavaFX {
     requires javafx.fxml;
     requires javafx.graphics;
+
+    opens com.xingray.javafx.base;
+    exports com.xingray.javafx.base;
+
+    opens com.xingray.javafx.util;
+    exports com.xingray.javafx.util;
 }
